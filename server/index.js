12967363f4e7714846d4fb9cd6bae1a562fa6a42
Geovany <marlongeo1999@gmail.com>
2022-10-26 22:13:55 -0600

    added env files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4001;
+const BASE_URL = process.env.NODE_MONGO_URL;
 
 app.listen(PORT, () => {
-  console.log("server started on PORT: ", PORT);
+  console.log("Server started on PORT:", PORT);
 });
+
+mongoose
+  .connect(BASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection good 🦸");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
