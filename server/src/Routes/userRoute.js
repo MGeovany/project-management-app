9ab098122cbae55c0955f4599f24ffcd76ec452c
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-10-28 21:19:31 -0600

    cambios de refactorizacion

--- server/src/Routes/userRoute.js
+++ server/src/Routes/userRoute.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const userController = require('../Controllers/userController')
-const userRoute= express.Router();
+const express = require("express");
+const userController = require("../Controllers/userController");
+const userRoute = express.Router();
 
-userRoute.post("/register",userController.userRegister);
-userRoute.post("/authenticate",userController.userAuthenticate);
+userRoute.post("/register", userController.userRegister);
+userRoute.post("/authenticate", userController.userAuthenticate);
 
 module.exports = userRoute;
