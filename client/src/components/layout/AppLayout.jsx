2bcf914eab615a1a035adff44c196cbfab3173f6
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-11-15 21:47:41 -0600

    started logic for navbar

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,49 +1,56 @@
-import { Box } from '@mui/material'
-import { useState, useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import authUtils from '../../utils/authUtils'
-import Loading from '../common/Loading'
-import Sidebar from '../common/Sidebar'
-import { setUser } from '../../redux/features/userSlice'
+import { AppBar, Box, Typography, CssBaseline, Toolbar } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authUtils from "../../utils/authUtils";
+import Loading from "../common/Loading";
+import Sidebar from "../common/Sidebar";
+import { setUser } from "../../redux/features/userSlice";
+import { useLocation } from "react-router-dom";
 
 const AppLayout = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [loading, setLoading] = useState(true)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  console.log(location.pathname);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
-      const user = await authUtils.isAuthenticated()
+      const user = await authUtils.isAuthenticated();
       if (!user) {
-        navigate('/login')
+        navigate("/login");
       } else {
-        // save user
-        dispatch(setUser(user))
-        setLoading(false)
+        dispatch(setUser(user));
+        setLoading(false);
       }
-    }
-    checkAuth()
-  }, [navigate])
+    };
+    checkAuth();
+  }, [navigate]);
 
-  return (
-    loading ? (
-      <Loading fullHeight />
-    ) : (
-      <Box sx={{
-        display: 'flex'
-      }}>
-        <Sidebar />
-        <Box sx={{
-          flexGrow: 1,
-          p: 1,
-          width: 'max-content'
-        }}>
-          <Outlet />
-        </Box>
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            NAVBAR
+          </Typography>
+          <a href="/dashboard">dashboard</a>
+        </Toolbar>
+      </AppBar>
+      {location.pathname.includes("boards") && <Sidebar />}
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Outlet />
       </Box>
-    )
-  )
-}
+    </Box>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+      <p>5</p>
+      <p>6</p>
+      <p>7</p>
+      <p>8</p>
+    </div>
+  );
+};
