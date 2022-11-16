54567b41d29f73c73110dc372a677040689a7811
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-11-16 14:22:42 -0600

    added picture to auth layout

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,11 +1,14 @@
 const assets = {
   images: {
-    logoDark: require('./images/logo-dark.png'),
-    appIcon: require('./images/favicon.png')
+    logoDark: require("./images/logo-dark.png"),
+    appIcon: require("./images/favicon.png"),
+    favicon: require("./images/favicon.ico"),
+    mainLogo: require("./images/main-logo.png"),
+    projectFlow: require("./images/projectflow.png"),
   },
   colors: {
-    secondary: '#292929'
-  }
-}
+    secondary: "#292929",
+  },
+};
 
-export default assets
+export default assets;
