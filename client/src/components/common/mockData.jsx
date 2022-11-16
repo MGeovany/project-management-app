3eef562af050372588d86651c651c03c6c9661f9
Randell Sean Caballero <102177367+RandellSCaballero@users.noreply.github.com>
2022-11-16 15:53:57 -0600

    Agregado el Dashboard con MockData
    Se agrego el diseño base que tendra el Dashboard, como ejemplo se utilizo MockData.

---
+++ client/src/components/common/mockData.jsx
@@ -0,0 +1,53 @@
+
+const mockData = [
+    {
+        id: '1',
+        title: '🗒️To Do',
+        tasks: [
+            {
+                id: '2',
+                title: 'Aprender Javascript'
+            },
+            {
+                id: '3',
+                title: 'Aprender Git'
+            },
+            {
+                id: '4',
+                title: 'Aprender Python'
+            },
+        ]
+    },
+    {
+        id: '5',
+        title: '✏️In Progress',
+        tasks: 
+        [
+            {
+                id:'6',
+                title: 'Aprender CSS'
+            },
+            {
+                id:'7',
+                title: 'Aprender Angular'
+            }
+        ]
+    },
+    {
+        id: '8',
+        title: '✔️ Finished',
+        tasks:
+        [
+            {
+                id:'9',
+                title: 'Aprender React'
+            },
+            {
+                id:'10',
+                title: 'Aprender Typescript'
+            }
+        ]
+    }
+]
+
+export default mockData
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,46 @@
 import React from "react";
+import mockData from "../components/common/mockData";
+import { Box, Button, Divider, Typography } from '@mui/material'
+import '../css/DashboardStyles.css'
 
 export const Dashboard = () => {
   return (
-    <div>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
-      <p>6</p>
-      <p>7</p>
-      <p>8</p>
-    </div>
+    <Box sx={{
+      paddingTop:10
+    }}>
+      <Typography align="center" variant="h2" component='h2'>
+        This is the Dashboard
+      </Typography>
+      
+      <Box sx={{ 
+              display: 'flex',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent:'space-around'
+              }}>
+      {
+        mockData && mockData.map(data => {
+          return(
+            <Box className="ItemBox">
+              <Typography variant='h4' component='h3' key={data.id}>
+                {data.title}
+              </Typography>
+              <Divider/>
+              <Box sx={{
+                display:'flex',
+                flexDirection:'column',
+                alignItems:'center'
+              }}>
+                <Typography variant="h6" component='h6'>
+                  Number of Tasks: {data.tasks.length}
+                </Typography>
+                <Button>View More</Button>
+              </Box>
+            </Box>
+          )
+        })
+      }
+      </Box>
+    </Box>
   );
 };
