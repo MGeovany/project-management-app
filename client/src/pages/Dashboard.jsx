4cb16bbdd0c6b3c1e7f5d35ecf968d0ed1e88cb4
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2022-11-17 22:02:03 -0600

    now dashboard allow only 4 projects

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
 import "../css/DashboardStyles.css";
 import { useSelector } from "react-redux";
 
 export const Dashboard = () => {
   const boards = useSelector((state) => state.board.value);
-
+  const boardCard = boards?.slice(4);
+  console.log(boardCard);
   return (
     <Box
       sx={{
@@ -21,43 +22,42 @@ export const Dashboard = () => {
         Dashboard
       </Typography>
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-around",
-        }}
+      <Grid
+        container
+        spacing={{ xs: 2, sm: 2, md: 1, xl: 1 }}
+        columns={{ xs: 2, sm: 2, md: 8, xl: 16 }}
       >
         {boards &&
-          boards.map((data, i) => {
+          boards?.slice(0, 4).map((data, i) => {
             return (
-              <Box className="ItemBox" key={i}>
-                <Typography
-                  variant="h5"
-                  component="h3"
-                  key={data.id}
-                  sx={{ textAlign: "center" }}
-                >
-                  {data.icon + " " + data.title}
-                </Typography>
-                <Divider />
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <Typography variant="body2" sx={{ paddingTop: 2 }}>
-                    Number of Tasks: {i > 0 ? 1 : 5}
+              <Grid item xs={2} sm={4} md={4} key={i}>
+                <Box className="ItemBox" key={i}>
+                  <Typography
+                    variant="h5"
+                    component="h3"
+                    key={data.id}
+                    sx={{ textAlign: "center" }}
+                  >
+                    {data.icon + " " + data.title}
                   </Typography>
-                  <Button>View More</Button>
+                  <Divider />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography variant="body2" sx={{ paddingTop: 2 }}>
+                      Number of Tasks: {i > 0 ? 1 : 5}
+                    </Typography>
+                    <Button>View More</Button>
+                  </Box>
                 </Box>
-              </Box>
+              </Grid>
             );
           })}
-      </Box>
+      </Grid>
     </Box>
   );
 };
