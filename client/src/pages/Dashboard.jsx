51bba2280438db2115ad675d636a0e3abe32dbab
Randell Sean Caballero <102177367+RandellSCaballero@users.noreply.github.com>
2022-11-17 22:25:11 -0600

    Agregados elementos al Dashboard
    Se agregaron elementos al dashboard, se instalaron dependencias para hacer funcionar ambos graficos.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography, Paper } from "@mui/material";
+import {
+  ArgumentAxis,
+  ValueAxis,
+  Chart,
+  BarSeries,
+} from '@devexpress/dx-react-chart-material-ui';
 import "../css/DashboardStyles.css";
 import { useSelector } from "react-redux";
+import ChartConversionRates from '../components/chart/ChartConversionRates'
 
 export const Dashboard = () => {
   const boards = useSelector((state) => state.board.value);
+  const data = [
+    { argument: 'Untitled', value: 6 },
+    { argument: 'Prueba', value: 3 },
+    { argument: 'Untitled 2', value: 3 },
+  ];
 
   return (
     <Box
       sx={{
         paddingTop: 10,
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
       }}
     >
       <Typography
@@ -57,6 +73,41 @@ export const Dashboard = () => {
               </Box>
             );
           })}
+      </Box>
+      <Box sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}>
+        <Box className="GraphBox">
+        <Typography
+          align="left"
+          variant="h6"
+          component="h2"
+          sx={{ paddingBottom: 2 }}
+        >
+          Section Progress
+        </Typography>
+        <Paper>
+            <Chart width={500} height={400} data={data}>
+              <ArgumentAxis />
+              <ValueAxis />
+              <BarSeries  valueField="value" argumentField="argument" barWidth={0.3} color={'gray'} />
+            </Chart>
+        </Paper>
+      </Box>
+        <Box className="SideGraphBox" width={700}>
+          <ChartConversionRates
+            title="Board Activity"
+            subheader="Test Project"
+            chartData={[
+              { label: 'To Do', value: 20 },
+              { label: 'In Progress', value: 30 },
+              { label: 'Completed', value: 37 }
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
