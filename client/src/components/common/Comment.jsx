59d2f990fc45333ae0ac2a69b102ea2314a20d3c
Geovany <marlongeo1999@gmail.com>
2022-12-03 16:44:23 -0600

    fixed user on blogs

--- client/src/components/common/Comment.jsx
+++ client/src/components/common/Comment.jsx
@@ -1,25 +1,20 @@
-import { Avatar, Box, IconButton, Tooltip } from "@mui/material";
-import React from "react";
-import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
-import EditIcon from '@mui/icons-material/Edit';
-import { useDispatch, useSelector } from "react-redux";
-import parse from 'html-react-parser'
-import '../../css/custom-blogs.css'
-import { setBlogs } from "../../redux/features/blogSlice";
-import { CommentForm } from "./CommentForm";
-import blogApi from "../../api/blogApi";
+import { Avatar, Box, IconButton, Tooltip } from "@mui/material"
+import React from "react"
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined"
+import EditIcon from "@mui/icons-material/Edit"
+import { useDispatch, useSelector } from "react-redux"
+import parse from "html-react-parser"
+import "../../css/custom-blogs.css"
+import { setBlogs } from "../../redux/features/blogSlice"
+import { CommentForm } from "./CommentForm"
+import blogApi from "../../api/blogApi"
 
-export const Comment = ({
-    comment, 
-    data,
-    date,
-    currentUserId
- }) => {
-  const user = useSelector((state) => state.user.value);
-  const blog = useSelector((state) => state.blog.value);
-  const createdAt = new Date(date).toLocaleDateString();
-  const dispatch = useDispatch();
-  
+export const Comment = ({ comment, data, date, currentUserId }) => {
+  const user = useSelector((state) => state.user.value)
+  const blog = useSelector((state) => state.blog.value)
+  const createdAt = new Date(date).toLocaleDateString()
+  const dispatch = useDispatch()
+
   const deleteBlog = async () => {
     try {
       await blogApi.delete()
@@ -31,31 +26,35 @@ export const Comment = ({
   }
   return (
     <>
-    <Box className="comment">
+      <Box className="comment">
         <Box className="comment-image-container">
-            <Tooltip title="Profile Picture">
-            <IconButton sx={{ p: 0, marginRight:2 }}>
-            <Avatar>{user.username.charAt(0)}</Avatar>
+          <Tooltip title="Profile Picture">
+            <IconButton sx={{ p: 0, marginRight: 2 }}>
+              <Avatar>{data.user.username.charAt(0)}</Avatar>
             </IconButton>
-        </Tooltip>
+          </Tooltip>
         </Box>
         <Box className="comment-right-part">
+          <Box className="comment-content">
             <Box className="comment-content">
-                <Box className="comment-content">
-                    <Box className="comment-author">{user.username}</Box>
-                    <Box>{createdAt}</Box>
-                </Box>
-                <Box className="comment-content">
-                    <IconButton variant="outlined" color="error" > {/*onClick={deleteBlog}*/}
-                    <DeleteOutlinedIcon />
-                    </IconButton>
-                    <IconButton color="primary"> {/*onClick={'Delete'}/>*/}
-                    <EditIcon/>
-                    </IconButton>
-                </Box>
+              <Box className="comment-author">{data.user.username}</Box>
+              <Box>{createdAt}</Box>
             </Box>
-            <Box className="comment-text">{parse(comment)}</Box>
-            {/* {isEditing && (
+            <Box className="comment-content">
+              <IconButton variant="outlined" color="error">
+                {" "}
+                {/*onClick={deleteBlog}*/}
+                <DeleteOutlinedIcon />
+              </IconButton>
+              <IconButton color="primary">
+                {" "}
+                {/*onClick={'Delete'}/>*/}
+                <EditIcon />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box className="comment-text">{parse(comment)}</Box>
+          {/* {isEditing && (
                 <CommentForm 
                 submitLabel='Update' 
                 hasCancelButton 
@@ -66,7 +65,7 @@ export const Comment = ({
                 />
             )} */}
         </Box>
-    </Box>
+      </Box>
     </>
-  );
+  )
 }
--- client/src/components/common/Comments.jsx
+++ client/src/components/common/Comments.jsx
@@ -1,61 +1,70 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { Box, Divider } from '@mui/material'
-import { getComments as getCommentsApi,
-         createComment as createCommentApi,
-         updateComment as updateCommentApi,
-         deleteComment as deleteCommentApi 
-} from '../../api/commentsApi'
-import { Comment } from "./Comment";
-import { CommentForm } from './CommentForm'
-import blogApi from "../../api/blogApi";
-import { setBlogs } from "../../redux/features/blogSlice";
-import { useSelector, useDispatch } from "react-redux";
-import '../../css/custom-blogs.css'
+import React from "react"
+import { useState, useEffect } from "react"
+import { Box, Divider } from "@mui/material"
+import {
+  getComments as getCommentsApi,
+  createComment as createCommentApi,
+  updateComment as updateCommentApi,
+  deleteComment as deleteCommentApi
+} from "../../api/commentsApi"
+import { Comment } from "./Comment"
+import { CommentForm } from "./CommentForm"
+import blogApi from "../../api/blogApi"
+import { setBlogs } from "../../redux/features/blogSlice"
+import { useSelector, useDispatch } from "react-redux"
+import "../../css/custom-blogs.css"
 
 export const Comments = ({ currentUserId }) => {
-    const [backendComments, setBackendComments] = useState([]);
-    const [activeComment, setActiveComment] = useState(null)
-    const dispatch = useDispatch();
-    const blog = useSelector(state => state.blog.value)
+  const [backendComments, setBackendComments] = useState([])
+  const [info, setInfo] = useState([])
+  const [activeComment, setActiveComment] = useState(null)
+  const dispatch = useDispatch()
+  const blog = useSelector((state) => state.blog.value)
 
-    useEffect(() => {
-      const getBlogs = async () => {
-        try{
-          const res = await blogApi.getAll();
-          dispatch(setBlogs(res));
-        } catch(err) {
-          alert(err);
-        }
-      };
-      getBlogs();
-    }, [dispatch]);
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const res = await blogApi.getAll()
+        dispatch(setBlogs(res))
+        setInfo(res)
+      } catch (err) {
+        alert(err)
+      }
+    }
+    getBlogs()
+  }, [dispatch])
 
-    useEffect(() => {
-        getCommentsApi().then((data) => {
-            setBackendComments(data);
-        })
-    }, [])
+  useEffect(() => {
+    getCommentsApi().then((data) => {
+      setBackendComments(data)
+    })
+  }, [])
+
+  console.log(backendComments)
   return (
     <Box className="comments">
-        <CommentForm submitLabel="Submit"/>
-        <Box className="comments-container">
-            {blog.length > 0 && blog.map((item, i) => (
-                <Box key={i}>
-                  <Divider sx={{
-                    marginBottom: 5,
-                  }}/>
-                  <Comment 
-                  key={item.id}
-                  comment={item.content} 
-                  date={item.date}
-                  data={item}
-                  currentUserId={item.user.id}
-                  />
-                  <Divider/>
-                </Box>
-            ))}
-        </Box>
+      <CommentForm submitLabel="Submit" />
+      <Box className="comments-container">
+        {blog.length > 0 &&
+          blog.map((item, i) => (
+            <Box key={i}>
+              <Divider
+                sx={{
+                  marginBottom: 5
+                }}
+              />
+              <Comment
+                info={info}
+                key={item.id}
+                comment={item.content}
+                date={item.date}
+                data={item}
+                currentUserId={item.user.id}
+              />
+              <Divider />
+            </Box>
+          ))}
+      </Box>
     </Box>
-  );
+  )
 }
