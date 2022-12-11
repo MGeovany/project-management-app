6d365994ffa22af51261a3321bbb12a1ec7bb145
Geovany <marlongeo1999@gmail.com>
2022-12-11 13:04:23 -0600

    formatted blogs

--- server/src/v1/controllers/blog.js
+++ server/src/v1/controllers/blog.js
@@ -1,50 +1,45 @@
-const Blog=require('../models/blog')
+const Blog = require("../models/blog");
 
 exports.create = async (req, res) => {
-  const content= req.body.content
-    try {
-      const blog = await Blog.create(
-        {
-          user: req.user._id,
-          content:content
-        }
-      )
-      res.status(201).json(blog)
-    } catch (err) {
-      console.log(err)
-      res.status(500).json(err)
-    }
+  const content = req.body.content;
+  try {
+    const blog = await Blog.create({
+      user: req.user._id,
+      content: content,
+    });
+    res.status(201).json(blog);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
+};
 
-  exports.delete = async (req,res) =>{
-    const {blogId} = req.params;
-    try {
-        const blog = await Blog.findByIdAndRemove(blogId);
-        res.status(201).json(blog)
-    } catch (err) {
-        res.status(500).json(err)
-    }
+exports.delete = async (req, res) => {
+  const { blogId } = req.params;
+  try {
+    const blog = await Blog.findByIdAndRemove(blogId);
+    res.status(201).json(blog);
+  } catch (err) {
+    res.status(500).json(err);
   }
+};
 
-  exports.update = async (req, res) => {
-    const { blogId } = req.params
-    const newUpdate=req.body
-    try {
-      const blog = await Blog.findByIdAndUpdate(
-        blogId,newUpdate 
-      )
-      res.status(200).json(blog)
-    } catch (err) {
-      res.status(500).json(err)
-    }
+exports.update = async (req, res) => {
+  const { blogId } = req.params;
+  const newUpdate = req.body;
+  try {
+    const blog = await Blog.findByIdAndUpdate(blogId, newUpdate);
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(500).json(err);
   }
+};
 
-  exports.getAll = async (req,res)=>{
-    try {
-      const blogs = await Blog.find().sort('-position')
-      res.status(200).json(blogs)
-    } catch (err) {
-      res.status(500).json(err)
-    }
-  
+exports.getAll = async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort("-position");
+    res.status(200).json(blogs);
+  } catch (err) {
+    res.status(500).json(err);
   }
+};
